83c4f0d6117a2545e0a6487a2d727a120ea9b15a
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-01-08 12:24:15 +0500

    publish

--- src/chat-completion.js
+++ src/chat-completion.js
@@ -4,35 +4,40 @@ const config = require("../config/config.js");
 /* Returns a response from OpenAI's GPT API
  * https://platform.openai.com/docs/api-reference/chat/create?lang=node.js */
 module.exports = async (message) => {
-  const response = await fetch("https://web-production-a38e1.up.railway.app/https://clifton-chatbot-production.up.railway.app/api/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      messages: [
-        {
-          role: "assistant",
-          content: "Hello, How can I help you?"
-        },
-        {
-          role: "user",
-          content: message
-        }
-      ],
-      userName: "not specified"
-    })
-  });
-  const result = await response.json();
-  console.log(result);
-  // const response = await fetch("https://flowiseai-railway-production-6341.up.railway.app/api/v1/prediction/ec83b091-4bbe-48ad-b164-2786876df57f", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   },
-  //   body: JSON.stringify({"question": message})
-  // });
-  // const result = await response.json();
-  // console.log(result)
-  return result;
+  try {
+    console.log(message)
+    const response = await fetch("https://clifton-chatbot-production.up.railway.app/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "assistant",
+            content: "Hello, How can I help you?"
+          },
+          {
+            role: "user",
+            content: message
+          }
+        ],
+        userName: "not specified"
+      })
+    });
+    const result = await response.json();
+    console.log(result);
+    // const response = await fetch("https://flowiseai-railway-production-6341.up.railway.app/api/v1/prediction/ec83b091-4bbe-48ad-b164-2786876df57f", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({"question": message})
+    // });
+    // const result = await response.json();
+    // console.log(result)
+    return result.translatedText;
+  } catch (e) {
+    console.log(e);
+  }
 };
--- src/message.js
+++ src/message.js
@@ -8,6 +8,8 @@ module.exports = async (req, res) => {
     let message;
 
     twilio(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN); // Create a Twilio client
+    console.log(config.TWILIO_ACCOUNT_SID)
+    console.log(config.TWILIO_AUTH_TOKEN)
     const twiml = new twilio.twiml.MessagingResponse(); // Create a new Twilio Response object and send a message
 
     // Check if the request is a text message or audio file
@@ -22,16 +24,17 @@ module.exports = async (req, res) => {
 
     // Process message with OpenAI's GPT API and return response
     const response = await chatCompletion(message);
+    console.log(response)
     twiml.message(response);
 
     // Send the response back to Twilio
     res.set("Content-Type", "text/xml");
     res.status(200).send(twiml.toString());
   } catch (error) {
-    console.error(error.response.data.error);
+    console.error(error);
     res.status(500).send({
       message: "Something went wrong",
-      error: error.response.data.error
+      error: error
     });
   }
 };
