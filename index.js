ee3aba98de7a6e3dc82c16859ce16bc1a391372b
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-01-09 23:05:22 +0500

    qqq

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions/v2");
 const app = require("./src/app");
 let { APP_PORT, REGION } = require("./config/config");
-APP_PORT = APP_PORT || 3000;
+// APP_PORT = APP_PORT || 3000;
 
-app.listen(APP_PORT, () => console.log(`Example app listening on port ${APP_PORT}!`));
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${process.env.PORT}!`));
 
 // Expose Express API as a single Cloud Function:
 // exports.app = functions.region(process.env.REGION).https.onRequest(app);
