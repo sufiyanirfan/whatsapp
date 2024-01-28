a7e960399ab2b38b0ee97b196fd249d8f11dc4bc
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2024-01-28 23:25:14 +0500

    ss

--- src/chat-completion.js
+++ src/chat-completion.js
@@ -6,37 +6,38 @@ const config = require("../config/config.js");
 module.exports = async (message) => {
   try {
     console.log(message);
-    // const response = await fetch("https://clifton-chatbot-production.up.railway.app/api/chat", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify({
-    //     messages: [
-    //       {
-    //         role: "assistant",
-    //         content: "Hello, How can I help you?"
-    //       },
-    //       {
-    //         role: "user",
-    //         content: message
-    //       }
-    //     ],
-    //     userName: "not specified"
-    //   })
-    // });
-    // const result = await response.json();
-    const response = await fetch("https://flowiseai-railway-production-4aac.up.railway.app/api/v1/prediction/820db25e-c4e0-4e70-b3ad-5b21946d49df", {
+    const response = await fetch("https://clifton-chatbot-production.up.railway.app/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ question: message })
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "assistant",
+            content: "Hello, How can I help you?"
+          },
+          {
+            role: "user",
+            content: message
+          }
+        ],
+        userName: "not specified"
+      })
     });
     const result = await response.json();
-    console.log(result);
-    return result.text;
-    // return result.translatedText;
+
+    // const response = await fetch("https://flowiseai-railway-production-4aac.up.railway.app/api/v1/prediction/820db25e-c4e0-4e70-b3ad-5b21946d49df", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({ question: message })
+    // });
+    // const result = await response.json();
+    // console.log(result);
+    // return result.text;
+    return result.translatedText;
   } catch (e) {
     console.log(e);
   }
